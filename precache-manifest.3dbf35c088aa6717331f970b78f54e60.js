bf313b15a4859eb59f03bffcee0db479643d8166
rizalaslam <admin@rizal.info>
2020-04-25 02:44:14 +0700

    Updates

---
+++ precache-manifest.3dbf35c088aa6717331f970b78f54e60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfda12c109e0cfcb31c264d12dd2515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4460af3812686fdaa5c6",
+    "url": "/static/css/2.4ded7582.chunk.css"
+  },
+  {
+    "revision": "0ea8334e1e8c3667131d",
+    "url": "/static/css/main.ef28d83c.chunk.css"
+  },
+  {
+    "revision": "4460af3812686fdaa5c6",
+    "url": "/static/js/2.667610c3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.667610c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea8334e1e8c3667131d",
+    "url": "/static/js/main.78da2a7e.chunk.js"
+  },
+  {
+    "revision": "8c3d95be2c1bbfc7c192",
+    "url": "/static/js/runtime-main.4eee303e.js"
+  },
+  {
+    "revision": "bcdeff85e2702ae3783ad5d0da15e098",
+    "url": "/static/media/example.bcdeff85.png"
+  }
+]);
